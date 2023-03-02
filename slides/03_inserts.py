c245c5c0460ac83cd58cf33a0fe12700ef344b72
Mike Bayer <mike_mp@zzzcomputing.com>
2023-03-02 14:26:46 -0500

    dev

--- slides/03_inserts.py
+++ slides/03_inserts.py
@@ -6,11 +6,13 @@ def print(text):
     _print(termcolor.colored(text, attrs=["bold"]))
 
 
-### slide::
+### slide:: b
 ### title:: Inserts
+### * In this section, we'll illustrate the ``insert()`` construct, which directly corresponds to a SQL INSERT statement
+### * We will start with an ORM-Centric Metadata model that is mostly the same as that of the previous section.
 
-# Start with similar tables as before.  One change is how we are doing the
-# default datetime for created_at, adding a SQL-time insert default.
+### slide:: b
+### title:: Inserts - ORM-Centric Model
 
 from datetime import datetime
 from typing import Optional
@@ -32,10 +34,11 @@ class User(Base):
         server_default=func.now()
     )
 
+### slide:: bi
+### * The above model features one change, which is a **server default** set up on the created_at column
 
-
-### slide:: p
-### title:: Create a database and tables
+### slide:: bp
+### title:: Inserts - Get an Engine and create tables
 
 from sqlalchemy import create_engine
 
@@ -44,34 +47,61 @@ engine = create_engine("sqlite://")
 with engine.begin() as conn:
     Base.metadata.create_all(conn)
 
+### slide:: bi
+### * Above, we can see the server default of "CURRENT_TIMESTAMP" generated for the created_at column
+### * This will allow new date values to be generated for any INSERT statement encountered by the server which doesn't already include this column
 
-### slide::
-### title:: Inserting some Rows
-# The insert() construct can be used against a mapped entity or Core
-# Table construct to produce an INSERT statement.
+
+### slide:: b
+### title:: The insert() construct
+### * The ``insert()`` function starts with a single argument; the table, or table-representing ORM entity, that is the target of the INSERT.
+### * In this case it's the User class
+### * The ``Table`` object that is part of the User class will be used behind the scenes to build a SQL INSERT statement
 
 from sqlalchemy import insert
 
-insert_stmt = insert(User).values(name="spongebob", fullname="Spongebob Squarepants")
+insert_stmt = insert(User)
+
+### slide:: bi
+### * Next, we further indicate a VALUES clause to be included in the INSERT statement, using the ``.values()`` method
+
+insert_stmt = insert_stmt.values(name="spongebob", fullname="Spongebob Squarepants")
+
+### slide:: b
+### title:: The insert() construct
+### * With that, we can stringify this object to illustrate the general SQL structure we'll be sending to the database.
 
 print(insert_stmt)
 
-### slide:: p
-### title:: Inserting some Rows
-# feed the INSERT statement to an execute() method to run it
+### slide:: bi
+### * The actual data is inside the ``insert()`` object, and will be passed to the database as parameters
+### * We can see the data in this case using a utility method called ``.compile()``
 
-insert_stmt = insert(User).values(name="spongebob", fullname="Spongebob Squarepants")
+print(insert_stmt.compile(compile_kwargs={"literal_binds": True}))
+
+
+### slide:: bp
+### title:: Executing the insert statement
+### * We use ``engine.begin()`` to create a transaction block that will commit at the end
+### * Then in the block, ``.execute()`` the statement
+
 
 with engine.begin() as conn:
     conn.execute(insert_stmt)
 
 
-### slide:: p
-### title:: Inserting many rows at once
-# The typical way we pass parameters to DML statements like INSERT
-# is to send the "parameters" to the execute() method directly.  SQLAlchemy
-# figures out what parameters to include in this mode, and knows a few ways to
-# make this run more efficiently than just row-at-a-time
+### slide:: b
+### title:: INSERT using "executemany"
+### * The previous example used ``.values()`` which produces a specific VALUES clause.
+### * For simple cases, we usually rely upon SQLAlchemy itself to generate this for us
+### * SQLAlchemy generates the VALUES clause, if not otherwise given, based on a collection of parameters passed to the ``.execute()`` method separately
+### * Then, we can pass a **dictionary** or **list of dictionaries** to the ``.execute()`` method to accompany the statement
+### * SQLAlchemy will then INSERT lists of dictionaries in bulk
+
+### slide:: b
+### title:: INSERT using "executemany"
+### * The automatic generation of VALUES works by passing dictionaries with string keys that match the names of the columns (more specifically, the key under which the column is part of the ``Table`` or ORM mapped class)
+### * This is one of relatively few places in SQLAlchemy where an API relies upon a naming convention
 
 with engine.begin() as conn:
     conn.execute(
@@ -79,15 +109,15 @@ with engine.begin() as conn:
         [
             {"name": "sandy", "fullname": "Sandy Cheeks"},
             {"name": "gary", "fullname": "Gary the Snail"},
-
         ],
     )
 
 
-### slide:: p
+### slide:: bp
 ### title:: Bonus Slide: SQLAlchemy 2.0 is very good at RETURNING
-# in SQLAlchemy 2.0, most backends can deliver rows for us from an
-# INSERT statement with the returned values.
+### * There's more INSERT can do
+### * But here we will just illustrate a fairly exciting new feature in SQLAlchemy 2.0
+### * Insert lots of rows with RETURNING at the same time (on almost every backend)
 
 with engine.begin() as conn:
     result = conn.execute(
